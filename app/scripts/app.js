24931db9df697cc9a07b49b4bf67e65f622bdec0
Fernando Espino I <mferna.92@gmail.com>
2016-01-28 18:35:10 -0600

    AngularJS

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+(function () {
+  'use strict';
+  // Aquí irá la funcionalidad
+  angular.module('blog', ['ngRoute','blog.controllers','blog.templates']);
+
+  function config ($locationProvider, $routeProvider) {
+    
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+    
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/post-list.tpl.html',
+        controller: 'PostListCtrl',
+        controllerAs: 'postlist'
+      })
+      .when('/post/:postId', {
+        templateUrl: 'views/post-detail.tpl.html',
+        controller: 'PostDetailCtrl',
+        controllerAs: 'postdetail'
+      })
+      .when('/new', {
+        templateUrl: 'views/post-create.tpl.html',
+        controller: 'PostCreateCtrl',
+        controllerAs: 'postcreate'
+      }); 
+    }
+
+    angular
+     .module('blog')
+     .config(config);
+
+})();
